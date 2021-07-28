c4ec3350cebe84f4a81b4dee791f240cf1438ecb
Kayla Swartzlander <swartzlander20@gmail.com>
2021-07-28 13:10:27 -0500

    Added CallingCard class and subfunctions to CallingCard.js

---
+++ CallingCard.js
@@ -0,0 +1,33 @@
+class CallingCard {
+    constructor (centsPerMinute) {
+        this.centsPerMinute = centsPerMinute
+        // this.totalAmount = 0;
+        this.totalMinutes = 0;
+      }
+  
+    addDollars(dollar) {
+     const amount = (dollar * 100);
+     const minute = (amount / this.centsPerMinute);
+     return this.totalMinutes += minute;
+  
+    }
+  
+    getRemainingMinutes () {
+    //  this.totalMinutes = (this.totalAmount / this.centsPerMinute);
+     return this.totalMinutes;
+    }
+  
+    useMinutes(amount) {
+     if ((this.totalMinutes - amount) > 0) {
+        this.totalMinutes -= amount;
+      } else {
+        this.totalMinutes = 0;
+      }
+  
+      return this.totalMinutes;
+    
+    }
+  
+  
+  }
+ export default CallingCard;
